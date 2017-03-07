724615fe6b5dd6c67f3efec5a05f5b46e7c75ff5
Zachary Clute <zach@Richards-iMac.local>
2017-03-07 18:17:31 -0500

    working docker compose but direcity is copied rather than mounted

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "ekko",
+    name: "ekko-host",
     dependencies: [
       .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
     ]
